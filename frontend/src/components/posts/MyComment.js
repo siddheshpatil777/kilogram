61e40c6227e31e894170a97a7e9fd06169fa46ea
siddheshpatil777 <siddheshpatil777@gmail.com>
2021-11-05 12:53:53 +0400

    comments looks clean with liked button

--- frontend/src/components/posts/MyComment.js
+++ frontend/src/components/posts/MyComment.js
@@ -2,25 +2,67 @@ import {Paper} from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import {Fragment} from "react/cjs/react.production.min";
+import {makeStyles} from "@material-ui/core/styles";
+import {red} from "@material-ui/core/colors";
+import {Link} from "react-router-dom";
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import Button from "@material-ui/core/Button";
 
+const useStyles = makeStyles((theme) => ({
+    userName: {
+        fontWeight: "bold",
+        color: "black",
+        textDecoration: "none",
+    },
+    commentLiked:{
+        // color:"red",
+    },
+
+}));
 const MyComment = ({data, mapForTcom, level}) => {
+    let isLiked=true;
+    const classes = useStyles();
     let spacing = "";
-
-    for (let i = 0; i < level; i++) {
-        // spacing.push(>);
-        spacing += "====";
+    if (level == 1) {
+        spacing = (<Fragment>&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>);
     }
-
     return (
         <div>
-            <Grid container direction="row" alignItems="center">
-                 <Typography fontWeight="fontWeightMedium" item noWrap>
-                     {spacing}{data.author_name}
-                </Typography>
-                 <Typography fontWeight="fontWeightMedium" item noWrap>
-                  {data.content}
-                </Typography>
+            <Grid container direction="row">
+                <Grid item container xs={11}>
+                    <Typography fontWeight="fontWeightBold" item noWrap className={classes.userName}>
+                        {spacing}<Link className={classes.userName}
+                                       to={"/profile/" + data.author_name}>{data.author_name}</Link>&nbsp;
+
+                    </Typography>
+                    <Typography fontWeight="fontWeightMedium" item noWrap>
+                        {data.content}
+                    </Typography>
+                </Grid>
+                <Grid item container xs={1}>
+                    {(isLiked===true)?<FavoriteIcon color="secondary" classes={classes.commentLiked} fontSize="small"/>:<FavoriteBorderIcon item fontSize="small"/>}
+                </Grid>
+
+                {/*<Grid item container direction="row-reverse">*/}
+                {/*    <FavoriteBorderIcon item fontSize="small"/>*/}
+                {/*</Grid>*/}
             </Grid>
+            {/*<Grid container direction="row" justifyContent="flex-start">*/}
+
+            {/*    <Typography fontWeight="fontWeightBold" item noWrap className={classes.userName}>*/}
+            {/*        {spacing}<Link className={classes.userName}*/}
+            {/*                       to={"/profile/" + data.author_name}>{data.author_name}</Link>&nbsp;*/}
+
+            {/*    </Typography>*/}
+            {/*    <Typography fontWeight="fontWeightMedium" item noWrap>*/}
+            {/*        {data.content}*/}
+            {/*    </Typography>*/}
+            {/*    <Grid item container direction="row-reverse">*/}
+            {/*        <FavoriteBorderIcon item fontSize="small"/>*/}
+            {/*    </Grid>*/}
+            {/*</Grid>*/}
 
             {/*<Typography noWrap>*/}
             {/*    text1*/}
