38fb42164683b63f39809810fcdcc8afdebc5f4f
siddheshpatil777 <siddheshpatil777@gmail.com>
2021-11-06 16:53:38 +0400

    added time since comment posted

--- frontend/src/components/posts/MyComment.js
+++ frontend/src/components/posts/MyComment.js
@@ -12,22 +12,61 @@ import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
     userName: {
-        fontWeight: "bold",
+        fontWeight: "700",
         color: "black",
         textDecoration: "none",
     },
-    commentLiked:{
+    commentLiked: {
         // color:"red",
+    },
+    commentContent: {
+        // color:"red",
+         fontSize: "0.9rem",
+         fontWeight: "300",
+
+    },
+    timestamp: {
+        fontWeight: "100",
+        fontSize: "0.7rem",
     },
 
 }));
 const MyComment = ({data, mapForTcom, level}) => {
-    let isLiked=true;
+    let isLiked = true;
     const classes = useStyles();
     let spacing = "";
     if (level == 1) {
         spacing = (<Fragment>&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>);
     }
+    // console.log(data.date_posted);
+    let today = Date.now();
+    let commentDate = Date.parse(data.date_posted);
+    let elaspedTime = today - commentDate;
+    elaspedTime = elaspedTime / (1000.0 * 60.0);
+    let timeSinceCommentPosted = "";
+    if (elaspedTime < 1.0) {
+
+        timeSinceCommentPosted = "just now";
+    } else if (1.0 <= elaspedTime && elaspedTime < 60.0) {
+        elaspedTime = elaspedTime | 0;
+        timeSinceCommentPosted = elaspedTime + "minutes ago";
+    } else if (60.0 <= elaspedTime && elaspedTime < 1440.0) {
+        elaspedTime = elaspedTime / 60.0;
+        elaspedTime = elaspedTime | 0;
+        timeSinceCommentPosted = elaspedTime + "hours ago";
+    } else if (1440.0 <= elaspedTime && elaspedTime < 565110.0) {
+
+        elaspedTime = elaspedTime / 60.0;
+        elaspedTime = elaspedTime / 24.0;
+        elaspedTime = elaspedTime | 0;
+        timeSinceCommentPosted = elaspedTime + " days ago";
+    } else {
+        // elaspedTime=elaspedTime/60.0;
+        // elaspedTime=elaspedTime | 0;
+        timeSinceCommentPosted = "long time ago";
+    }
+
+
     return (
         <div>
             <Grid container direction="row">
@@ -35,15 +74,21 @@ const MyComment = ({data, mapForTcom, level}) => {
                     <Typography fontWeight="fontWeightBold" item noWrap className={classes.userName}>
                         {spacing}<Link className={classes.userName}
                                        to={"/profile/" + data.author_name}>{data.author_name}</Link>&nbsp;
-
                     </Typography>
-                    <Typography fontWeight="fontWeightMedium" item noWrap>
+                    <Typography fontWeight="fontWeightMedium" item noWrap className={classes.commentContent}>
+
                         {data.content}
                     </Typography>
                 </Grid>
                 <Grid item container xs={1}>
-                    {(isLiked===true)?<FavoriteIcon color="secondary" classes={classes.commentLiked} fontSize="small"/>:<FavoriteBorderIcon item fontSize="small"/>}
+                    {(isLiked === true) ?
+                        <FavoriteIcon color="secondary" classes={classes.commentLiked} fontSize="small"/> :
+                        <FavoriteBorderIcon item fontSize="small"/>}
                 </Grid>
+                <Typography fontWeight="fontWeightSmall" item noWrap className={classes.timestamp}>
+                    {spacing}{timeSinceCommentPosted}
+                </Typography>
+
 
                 {/*<Grid item container direction="row-reverse">*/}
                 {/*    <FavoriteBorderIcon item fontSize="small"/>*/}
